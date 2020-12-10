2c1edc4c296338edfc09dd1805aea8347aaef61e
gulyaeve <gulyaev.e@icloud.com>
2020-12-10 21:06:21 +0300

    Initial Commit

---
+++ TheLightUI/TheLightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheLightUIApp.swift
+//  TheLightUI
+//
+//  Created by Евгений Гуляев on 10.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct TheLightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
